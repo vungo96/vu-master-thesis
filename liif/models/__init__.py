98b6e82525d96c63d149e6e8bd03017d4416b975
Minh Vu Ngo <vu.ngo@rwth-aachen.de>
2022-12-29 19:21:38 +0100

    copied repos and modified liif

---
+++ liif/models/__init__.py
@@ -0,0 +1,10 @@
+from .models import register, make, make_layer, PixelShufflePack, default_init_weights, pixel_unshuffle
+from . import edsr, rdn, rcan
+from . import mlp
+from . import liif
+from . import misc
+from .rrdb_net import RRDB
+from . import glean_styleganv2
+from . import stylegan2
+from . import liif_glean_styleganv2
+from . import liif_glean_styleganv2_bank_encoder
