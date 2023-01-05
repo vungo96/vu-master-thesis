eaf113c76203b2ac0857affdd0897c93c74db8a9
Minh Vu Ngo <vu.ngo@rwth-aachen.de>
2023-01-05 08:49:21 -0500

    added log for tensorboard

--- liif/slurm/job_liif_glean.sh
+++ liif/slurm/job_liif_glean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ### Start of Slurm SBATCH definitions
-#SBATCH -c 4                # Number of cores (-c)
-#SBATCH -t 0-08:00          # Runtime in D-HH:MM, minimum of t minutes
+#SBATCH -c 8                # Number of cores (-c)
+#SBATCH -t 0-8:00          # Runtime in D-HH:MM, minimum of t minutes
 #SBATCH -p gpu_test              # Partition to submit to
 #SBATCH --mem-per-cpu=3900M #M is the default and can therefore be omitted, but could also be K(ilo)|G(iga)|T(era)
 #SBATCH --gres=gpu:1
@@ -18,9 +18,10 @@ cd /n/home08/mngo/vu-master-thesis/liif
 
 ### load modules
 module load Anaconda3/2020.11
-module load cuda/10.2.89-fasrc01
+module load cuda/11.6.2-fasrc01
+module load cudnn/8.5.0.96_cuda11-fasrc01
  
 ### beginning of executable commands
-source activate liif_glean_experiment_python3.7_a100_gpu
+source activate liif_glean_experiment_python3.7_torch1.12.0
 
 python train_liif.py --config configs/train-celebAHQ/train_celebAHQ-32-256_liif_glean_bank_encoder.yaml --gpu 1
