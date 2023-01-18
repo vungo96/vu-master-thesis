025f6839d25819661cec2106323c2e8bde087d99
Minh Vu Ngo <vu.ngo@rwth-aachen.de>
2023-01-18 13:47:03 +0100

    fixed runnign test on gpu

--- liif/test.py
+++ liif/test.py
@@ -115,8 +115,7 @@ if __name__ == '__main__':
     parser.add_argument('--gpu', default='0')
     args = parser.parse_args()
 
-    device = torch.device('cuda' if torch.cuda.is_available()
-                          and int(args.gpu) > 0 else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Run on device: ", device)
 
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
