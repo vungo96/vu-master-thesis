ed256831ff91b6f452788d15a77a6c40afa3a308
Minh Vu Ngo <vu.ngo@rwth-aachen.de>
2023-01-24 15:18:58 +0100

    added concatenating images for comparison

---
+++ liif/test_images/concat_images.py
@@ -0,0 +1,44 @@
+import os
+from PIL import Image
+from tqdm import tqdm
+
+save_folder = 'concatenated_images'
+image_folder1 = './_test-celebAHQ-32-256_liif'
+image_folder2 = './_test-celebAHQ-32-256_liif_glean_enhanced'
+size = 256
+# the number of epochs passed until we save images -> set in test.py
+num_epochs = 500
+
+filenames1 = sorted(os.listdir(image_folder1))
+filesnames2 = sorted(os.listdir(image_folder2))
+
+if len(filenames1) != len(filesnames2):
+    raise ValueError('Number of images in folders do not match')
+
+images1 = [Image.open(os.path.join(image_folder1, filename)).resize((size, size)) for filename in filenames1]
+images2 = [Image.open(os.path.join(image_folder2, filename)).resize((size, size)) for filename in filesnames2]
+
+total_width = size * 4
+max_height = size
+
+new_im = Image.new('RGB', (total_width, max_height))
+
+x_offset = 0
+counter = 0
+
+# stack together input -> gt -> pred1 -> pred2
+for i in range(len(filenames1)):
+    im = images1[i]
+    new_im.paste(im, (x_offset,0))
+    x_offset += im.size[0]
+
+    if i % 3 == 2:
+        im = images2[i]
+        new_im.paste(im, (x_offset,0))
+        x_offset += im.size[0]
+        
+        new_im.save(os.path.join('.', save_folder, str(counter*num_epochs) + '_stacked.png'))
+        new_im.close()
+        new_im = Image.new('RGB', (total_width, max_height))
+        x_offset = 0
+        counter += 1
