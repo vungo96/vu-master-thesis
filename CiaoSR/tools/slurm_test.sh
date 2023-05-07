1eafc8f6d55854c45d6cf29fb39f2bdd8e0eb1b4
Minh Vu Ngo <vu.ngo@rwth-aachen.de>
2023-05-07 17:36:18 -0400

    fixed test slurm script for ciaosr

--- CiaoSR/tools/slurm_test.sh
+++ CiaoSR/tools/slurm_test.sh
@@ -2,10 +2,11 @@
 
 ### Start of Slurm SBATCH definitions
 #SBATCH -c 14               # Number of cores (-c)
-#SBATCH -t 2-00:00          # Runtime in D-HH:MM, minimum of t minutes
-#SBATCH -p gpu              # Partition to submit to
+#SBATCH -t 1-00:00          # Runtime in D-HH:MM, minimum of t minutes
+#SBATCH -p gpu_requeue              # Partition to submit to
 #SBATCH --mem-per-cpu=20000M #M is the default and can therefore be omitted, but could also be K(ilo)|G(iga)|T(era)
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks=2
 #SBATCH -o tools/outputs/myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e tools/outputs/myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 
@@ -28,9 +29,10 @@ source activate ciaosr6
 export MASTER_PORT=$((12000 + $RANDOM % 20000))
 
 CONFIG=configs/ciaosr/001_localimplicitsr_edsr_div2k_g1_c64b16_1000k_unfold_lec_mulwkv_res_nonlocal.py
-GPUS=1
+GPUS=2
+CKPT=work_dirs/001_localimplicitsr_edsr_div2k_g1_c64b16_1000k_unfold_lec_mulwkv_res_nonlocal/latest.pth
 
 PYTHONPATH=/bin/..:tools/..:
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$MASTER_PORT \
-    ./tools/train.py $CONFIG --launcher pytorch
+    ./tools/test.py $CONFIG $CKPT --launcher slurm
--- CiaoSR/tools/slurm_test_new.sh
+++ CiaoSR/tools/slurm_test_new.sh
@@ -8,17 +8,17 @@ CONFIG=configs/ciaosr/001_localimplicitsr_edsr_div2k_g1_c64b16_1000k_unfold_lec_
 CHECKPOINT=work_dirs/001_localimplicitsr_edsr_div2k_g1_c64b16_1000k_unfold_lec_mulwkv_res_nonlocal/latest.pth
 GPUS=${GPUS:-4}
 GPUS_PER_NODE=${GPUS_PER_NODE:-4}
-CPUS_PER_TASK=${CPUS_PER_TASK:-5}
+CPUS_PER_TASK=${CPUS_PER_TASK:-8}
 PY_ARGS=${@:5}
 SRUN_ARGS=${SRUN_ARGS:-""}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 srun -p ${PARTITION} \
+    -t 1-00:00 \
+    -c 10 \
     --job-name=${JOB_NAME} \
-    --gres=gpu:${GPUS_PER_NODE} \
     --ntasks=${GPUS} \
-    --ntasks-per-node=${GPUS_PER_NODE} \
-    --cpus-per-task=${CPUS_PER_TASK} \
+    --mem-per-cpu=20000M \
     --kill-on-bad-exit=1 \
     ${SRUN_ARGS} \
     python -u tools/test.py ${CONFIG} ${CHECKPOINT} --launcher="slurm" ${PY_ARGS}
