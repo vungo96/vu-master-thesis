1f2e4ef2922752160ec1aaca8f68a18a7f4655b9
Minh Vu Ngo <vu.ngo@rwth-aachen.de>
2023-08-29 09:03:01 -0400

    tikz distributions

--- liif/scripts/crop_image.py
+++ liif/scripts/crop_image.py
@@ -31,19 +31,19 @@ def crop_image(input_path, center_x, center_y, patch_size_x, patch_size_y, outpu
 
 if __name__ == "__main__":
     # Example usage:
-    #input_path = "../../../mngo_datasets/load/div2k/DIV2K_valid_HR/0806.png"
+    input_path = "../../../mngo_datasets/load/div2k/DIV2K_valid_HR/0806.png"
     #input_path = "../../../mngo_datasets/load/benchmark/Urban100/HR/img004.png"
-    input_path = "test_images/compare_crops/0806-x4-GT-marked.png"
+    #input_path = "test_images/compare_crops/0806-x4-GT-marked.png"
     #input_path = "test_images/interesting_comparisons2/0806-x4-ciaosr.png"
-    tag = "0806-x4-GT-big-crop"
+    tag = "0806-x4-GT"
     center_x = 420 # 1040 + 64 x18 0821 #400 monarch #128+32 set5 #1660 birds # 550 jaguar # 400 +128 crocodile # 360 + 64 manga109 # 175  div2k-0821
     center_y = 470 # 128 + 40 - 48  #256 + 30 #128 #815 # 1100 # 970 +128 -5 #64 # 860 
     patch_size_x = 128 # 128 + 20 #128 #128 #256 # 512 # 512 #128 # 256 
     patch_size_y = 128 # 128 + 20 #128 #128 #256 # 512 # 512 #128 #256
-    center_x = 600
-    center_y = 600
-    patch_size_x = 1000  
-    patch_size_y = 1000 
+    #center_x = 600
+    #center_y = 600
+    #patch_size_x = 1000  
+    #patch_size_y = 1000 
     output_folder = "test_images/compare_crops"  
 
     crop_image(input_path, center_x, center_y, patch_size_x, patch_size_y, output_folder, tag)
---
+++ liif/scripts/plot_scales_distribution_tikz.py
@@ -0,0 +1,94 @@
+import matplotlib.pyplot as plt
+import os
+import pickle
+import tikzplotlib  # Import the tikzplotlib module
+
+import matplotlib.ticker as ticker
+
+# Path to pickle files
+#path = 'save/_train_edsr-baseline-lte-variable-input-div2k-plot-distribution_plot_scale_distribution/'
+path = 'save/_train_swinir-baseline-lte-variable-input-div2k-final_sample-2304-scale-1toMax-inputs-48-div2k-edge-crop-batch-64-resume/'
+
+bins = 60
+
+# Set default font to Times New Roman
+plt.rcParams['font.family'] = 'Times New Roman'
+
+# Set default font size
+plt.rcParams['font.size'] = 14
+
+# Load dictionary from first file created via pickle
+with open(path + 'scale_freq.pickle', 'rb') as f:
+    scale_freq = pickle.load(f)
+
+# Sort dictionary keys
+sorted_keys = sorted(scale_freq.keys())
+
+# bins dependent on largest scale
+bins = max(scale_freq.keys()) + 5
+print('highest scale:', bins - 5)
+
+# Plot distribution of first file as a histogram
+plt.hist(sorted_keys, weights=[scale_freq[k] for k in sorted_keys], bins=bins, range=(0,bins))
+#plt.xlabel('Scale')
+#plt.ylabel('Frequency')
+#plt.title('Distribution of Scales')
+
+ax = plt.gca()
+formatter = ticker.ScalarFormatter(useMathText=True)
+formatter.set_powerlimits((-2, 2))  # Adjust the power limits if needed
+ax.yaxis.set_major_formatter(formatter)
+
+# Save plot of first file to same directory as pickle file
+plot_path = os.path.join(path, 'scale_distribution.png')
+plt.savefig(plot_path)
+
+# Plot distribution of first file as a histogram
+plt.hist(sorted_keys, weights=[scale_freq[k] for k in sorted_keys], bins=bins, range=(0,bins), edgecolor='black')
+ax = plt.gca()
+formatter = ticker.ScalarFormatter(useMathText=True)
+formatter.set_powerlimits((-2, 2))
+ax.yaxis.set_major_formatter(formatter)
+plt.yscale('log')  # Set y-axis scale to logarithmic
+
+# Save Matplotlib figure to TikZ code using tikzplotlib
+tikzplotlib.save(os.path.join(path, 'scale_distribution.tikz'))
+
+# Load dictionary from second file created via pickle
+with open(path + 'scale_max_freq.pickle', 'rb') as f:
+    max_scale_freq = pickle.load(f)
+
+# Sort dictionary keys
+sorted_keys = sorted(max_scale_freq.keys())
+
+# bins dependent on largest scale
+bins = max(max_scale_freq.keys()) + 5
+print('highest scale:', bins - 5)
+print('lowest scale:', min(max_scale_freq.keys()))
+
+# Plot distribution of second file as a histogram
+plt.clf() # Clear previous plot
+plt.hist(sorted_keys, weights=[max_scale_freq[k] for k in sorted_keys], bins=bins, range=(0,bins))
+# plt.xlabel('Scale')
+# plt.ylabel('Frequency')
+# plt.title('Distribution of Max Scales')
+
+ax = plt.gca()
+formatter = ticker.ScalarFormatter(useMathText=True)
+# formatter.set_powerlimits((-2, 2))  # Adjust the power limits if needed
+ax.yaxis.set_major_formatter(formatter)
+
+# Save plot of second file to same directory as pickle file
+plot_path = os.path.join(path, 'max_scale_distribution.png')
+plt.savefig(plot_path)
+
+# Plot distribution of second file as a histogram
+plt.clf() # Clear previous plot
+plt.hist(sorted_keys, weights=[max_scale_freq[k] for k in sorted_keys], bins=bins, range=(0,bins), edgecolor='black')
+ax = plt.gca()
+formatter = ticker.ScalarFormatter(useMathText=True)
+ax.yaxis.set_major_formatter(formatter)
+plt.yscale('log')  # Set y-axis scale to logarithmic
+
+# Save Matplotlib figure to TikZ code using tikzplotlib
+tikzplotlib.save(os.path.join(path, 'max_scale_distribution.tikz'))
