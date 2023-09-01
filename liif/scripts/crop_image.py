991dc740aa8214ffb9ea953c379a8e5ca0f03711
Minh Vu Ngo <vu.ngo@rwth-aachen.de>
2023-09-01 13:36:38 +0200

    added GT of butterfly and birds

--- liif/scripts/crop_image.py
+++ liif/scripts/crop_image.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import os
 
+
 def crop_image(input_path, center_x, center_y, patch_size_x, patch_size_y, output_folder, tag):
     # Open the image
     image = Image.open(input_path)
@@ -14,8 +15,8 @@ def crop_image(input_path, center_x, center_y, patch_size_x, patch_size_y, outpu
 
     # Calculate the coordinates for cropping the patch
     left = center_x - patch_size_x // 2
-    top = center_y - patch_size_y // 2 
-    right = left + patch_size_x 
+    top = center_y - patch_size_y // 2
+    right = left + patch_size_x
     bottom = top + patch_size_y
 
     # Crop the image
@@ -29,21 +30,38 @@ def crop_image(input_path, center_x, center_y, patch_size_x, patch_size_y, outpu
     output_path = os.path.join(output_folder, f"{tag}.png")
     cropped_image.save(output_path)
 
+
 if __name__ == "__main__":
     # Example usage:
-    input_path = "../../../mngo_datasets/load/div2k/DIV2K_valid_HR/0806.png"
+    #input_path = "../../../mngo_datasets/load/div2k/DIV2K_valid_HR/epoch-img-0_pred.png"
     #input_path = "../../../mngo_datasets/load/benchmark/Urban100/HR/img004.png"
-    #input_path = "test_images/compare_crops/0806-x4-GT-marked.png"
-    #input_path = "test_images/interesting_comparisons2/0806-x4-ciaosr.png"
-    tag = "0806-x4-GT"
-    center_x = 420 # 750 urban100 # 1040 + 64 x18 0821 #400 monarch #128+32 set5 #1660 birds # 550 jaguar # 400 +128 crocodile # 360 + 64 manga109 # 175  div2k-0821 x24
-    center_y = 470 # 500 # 128 + 40 - 48  #256 + 30 #128 #815 # 1100 # 970 +128 -5 #64 # 860 
-    patch_size_x = 128 # 200 # 128 + 20 #128 #128 #256 # 512 # 512 #128 # 256 
-    patch_size_y = 128 # 200 # 128 + 20 #128 #128 #256 # 512 # 512 #128 #256
-    #center_x = 600
-    #center_y = 600
-    #patch_size_x = 1000  
-    #patch_size_y = 1000 
-    output_folder = "test_images/compare_crops"  
+    scales = [2, 3, 4, 6, 8, 12, 18, 24, 30]
+    tags = ["lsdir-1to4-edge-crop", "lsdir-1to4-traditional", "lsdir-1toMax-edge-crop", "lsdir-1toMax",
+            "div2k-1toMax-edge-crop", "div2k-1toMax", "div2k-1to4-edge-crop", "div2k-1to4-traditional"]
 
-    crop_image(input_path, center_x, center_y, patch_size_x, patch_size_y, output_folder, tag)
+    for scale in scales:
+        for tag in tags:
+            input_path = "test_images-x/test_images-x"+str(scale)+"/_test-chosen-" + \
+                str(scale)+"_" + tag + "/epoch-img-28_pred.png"
+            input_path = "test-images-ciaosr/chosen-x" + \
+                str(scale) + "/butterfly.png"
+            tag = "ciaosr"
+            tag = "butterfly-x" + str(scale) + "-" + tag
+            folder = "**merge-butterfly"
+            # center_x = # 420 palms # 750 urban100 # 1040 + 64 x18 0821 #400 monarch #128+32 set5 #1660 birds # 550 jaguar # 400 +128 crocodile # 360 + 64 manga109 # 175  div2k-0821 x24
+            # center_y = # 470  # 500 # 128 + 40 - 48  #256 + 30 #128 #815 # 1100 # 970 +128 -5 #64 # 860
+            # patch_size_x = # 128  # 200 # 128 + 20 #128 #128 #256 # 512 # 512 #128 # 256
+            # patch_size_y = # 128  # 200 # 128 + 20 #128 #128 #256 # 512 # 512 #128 #256
+            center_x = 128
+            center_y = 128
+            patch_size_x = 245
+            # 128  # 200 # 128 + 20 #128 #128 #256 # 512 # 512 #128 #256
+            patch_size_y = patch_size_x
+            #center_x = 600
+            #center_y = 600
+            #patch_size_x = 1000
+            #patch_size_y = 1000
+            output_folder = "test_images/compare_crops/" + folder
+
+            crop_image(input_path, center_x, center_y, patch_size_x,
+                       patch_size_y, output_folder, str(scale) + "-" + tag)
